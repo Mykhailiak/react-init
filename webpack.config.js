66119ecc7d0c5ac3ac9708fe17c42340fedddf42
Yura Myhaylyak <ghost86421@gmail.com>
2017-05-22 23:43:44 +0300

    Setup HMR

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,22 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        loaders: ['react-hot-loader', 'babel-loader']
+      }
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
